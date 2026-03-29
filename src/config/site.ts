7b638cd93eb647dd84da468df412d28c5a805cc6
Joel Mencia <menciajoel@gmail.com>
2026-03-29 10:39:27 -0600

    chore(analytics): temporarily switch GA4 measurement ID for production validation

--- src/config/site.ts
+++ src/config/site.ts
@@ -19,7 +19,7 @@ export const SITE_CONFIG = {
   origin: "https://lunytales.com",
   hotmartUrlEs: "https://pay.hotmart.com/H100949723W?off=94i6su53",
   hotmartUrlEn: "https://pay.hotmart.com/K104828315K",
-  gaMeasurementId: "G-LNBS9K975K",
+  gaMeasurementId: "G-8RCWV9MWFQ",
   paths: {
     home: "",
     homeEn: "en/",
